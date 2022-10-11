6ab8dfb148a9aa57e6fde72bf1c7f3cc19436140
asadansari8840 <asadansari.aa786@gmail.com>
2022-10-11 13:49:51 +0530

    Updates

---
+++ precache-manifest.15f52d47a795b66f3edaa1e53a0653aa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7996d9bef0b213ad7ac2",
+    "url": "/TextUtils-React/static/js/main.7996d9be.chunk.js"
+  },
+  {
+    "revision": "48dc1c1c87d8faf3dbc4",
+    "url": "/TextUtils-React/static/js/1.48dc1c1c.chunk.js"
+  },
+  {
+    "revision": "9c38bf068e4b70cd6a20",
+    "url": "/TextUtils-React/static/js/runtime~main.9c38bf06.js"
+  },
+  {
+    "revision": "b04b9eb204130865d9a0380a153c2691",
+    "url": "/TextUtils-React/index.html"
+  }
+];
